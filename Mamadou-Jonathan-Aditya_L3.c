7508ccf2db6b80f2ab6c1419c084cf3e632e09a5
arahman27 <43826981+arahman27@users.noreply.github.com>
2018-11-22 17:06:36 -0500

    Update Mamadou-Jonathan-Aditya_L3.c

--- Mamadou-Jonathan-Aditya_L3.c
+++ Mamadou-Jonathan-Aditya_L3.c
@@ -31,18 +31,16 @@ I2C_2               leftIEM             Integrated Encoder    Encoted mounted on
 	const int right =
 */
 
-struct Data
+/*struct Data
 {
-		unsigned Forward;
-		unsigned Backwards;
-		unsigned Left;
-		unsigned Right;
-		unsigned ArmUp;
-		unsigned ArmDn;
-		unsigned GripOpen ;
-		unsigned GripClose;
+	short SonarValue;
+	short ArmValue;
+	bool Drive;
+	bool ArmDown;
+	bool ClawClosed;
+	int Batt;
+};*/
 
-};
 
 /*task UARTX_send{
 
@@ -64,31 +62,236 @@ struct Data
 
 }*/
 
-task UARTX_recv(){
+/*task sendData(){
+	sendChar(uartOne, 'a');
+}*/
 
-	Data rcvChar;		//Variable to hold the reciving byte data
-
-	while (true)
-	{
+char UARTX_recv(){
+	char rcvChar;
+	//char rcvChar = '\0';		//Variable to hold the reciving byte data
+	/*
+	while(){
 		rcvChar = getChar(uartOne);
-		if (rcvChar == -1)
+		writeDebugStream("rcvchar is: ", rcvChar);
+	}
+	*/
+	//while (true)
+	//{
+		rcvChar = getChar(uartOne);
+		return rcvChar;
+		/*if (rcvChar == -1)
 		{
-
+			writeDebugStream("rcvchar if it is == -1", rcvChar);
+			//printf("<%c>", rcvChar);
 			// No character available
 	 		wait1Msec(3);
 	 		continue;
 		}
-		if(rcvChar != 0)
+		if(rcvChar == 1)
 		{
-			motor[rightMotor] = 65;
-			motor[leftMotor] = 65;
-		}
-	}
+			writeDebugStream("rcvchar if it !=", rcvChar);
+			//motor[rightMotor] = 65;
+			//motor[leftMotor] = 65;
+		}*/
+	//}
 
 }
 
+	//directional variables for 'turnRobot' function
+	const int left = 1;
+	const int right = 0;
+
+	//speed is the variable given for how much power and in turn speed is given to drive the robot
+	int speed = 63;
+
+
+
+	//light value for line trackers
+  int threshold = 505;
+
+	//1 full rotation = 12.56 inch = 1.04feet ( using 627 as target )
+	const float rotation = 627;
+
+	//Should move forward until a line is recognized
+	/*void findLine()
+	{
+		if(SensorValue[lineTrackerCENTER] < 4000)
+		{
+	 		motor[leftMotor] = 63;
+	 		motor[rightMotor] = 0;
+	 	}
+		else
+		{
+	 	 motor[leftMotor] = 0;
+	 	 motor[rightMotor] = 0;
+	  }
+	}
+	*/
+	//follows line going at 1-arg power given until 2-arg distance
+	//in inches is deteched by the sonar
+	void followLineUntilObject(int power, int objDistance)
+	{
+	  /* surfaces, adding them together, then dividing by 2. */
+	  while(SensorValue(sonarSensor) > 6)
+	  {
+	  	if(SensorValue(sonarSensor) < 16)
+	  		power = power / 2;
+
+	    // RIGHT sensor sees dark:
+	    if(SensorValue(lineFollowerRIGHT) > threshold)
+	    {
+	      // counter-steer right:
+	      motor[leftMotor]  = power;
+	      motor[rightMotor] = 0;
+	    }
+	    // CENTER sensor sees dark:
+	    if(SensorValue(lineFollowerCENTER) > threshold)
+	    {
+	      // go straight
+	      motor[leftMotor]  = power;
+	      motor[rightMotor] = power;
+	    }
+	    // LEFT sensor sees dark:
+	    if(SensorValue(lineFollowerLEFT) > threshold)
+	    {
+	      // counter-steer left:
+	      motor[leftMotor]  = 0;
+	      motor[rightMotor] = power;
+	    }
+	    else
+	    {
+	    	motor[leftMotor]  = 0;
+	      motor[rightMotor] = 0;
+	    }
+	  }
+	}
+  void raiseArm(int power)
+	{
+			//brief delay
+			wait1Msec(2000);
+			bool arm = true;
+			int result = 0;
+			while (arm)
+			{
+				//potentiometer value to stop arm movement must be higher due to
+				//power delivered to the claw, momentum/gravity, and the weight of the object
+				//will cause arm to cut off at potentiometer value but drift lower for a longer time
+				if(SensorValue(potentiometerSensor) == 1600 || SensorValue(potentiometerSensor) > 1600)
+				{
+					arm = false;
+					motor[armMotor] = 0;
+				}
+				else
+				{
+
+					motor[armMotor] = -power;
+				}
+			}
+	}
+
+	void openClaw()
+	{
+		wait1Msec(2000);
+		motor[clawMotor] = 50;
+		wait1Msec(1100);
+	}
+
+	void closeClaw()
+	{
+		wait1Msec(2000);
+		motor[clawMotor] = -50;
+		wait1Msec(1100);
+	}
+
+	void lowerArm(int power)
+	{
+			//brief delay
+			wait1Msec(2000);
+			bool arm = true;
+			while (arm)
+			{
+				//potentiometer value to stop arm movement must be higher due to
+				//power delivered to the claw, momentum/gravity, and the weight of the object
+				//will cause arm to cut off at potentiometer value but drift lower for a longer time
+				if(SensorValue(potentiometerSensor) == 1599 || SensorValue(potentiometerSensor) < 1599)
+				{
+					arm = false;
+					motor[armMotor] = 0;
+				}
+				else
+				{
+					motor[armMotor] = 0;
+					motor[armMotor] = power;
+				}
+			}
+	}
+
+	void forwardMovement(double target, int power)
+	{
+		resetMotorEncoder(rightMotor);
+		resetMotorEncoder(leftMotor);
+
+		//result holds number in feet
+		double result = target * rotation;
+
+		moveMotorTarget(rightMotor,(int)result,power,true);
+		moveMotorTarget(leftMotor,(int)result,power,true);
+		wait1Msec(10000);
+
+		while((getMotorTargetCompleted(rightMotor) == false || getMotorTargetCompleted(leftMotor) == false)){}
+	}
+
+	// 90 degree turn = ~ 933 as target
+	void turnRobot(double target, int power, int direction)
+	{
+		resetMotorEncoder(rightMotor);
+		resetMotorEncoder(leftMotor);
+
+		//result holds number in feet
+		double result = target * rotation;
+
+		//left turn
+		if(direction == left)
+		{
+			moveMotorTarget(rightMotor,(int)result,power,true); //moveMotorTarget takes int for distance therefore the (int) casting
+			moveMotorTarget(leftMotor,0,0,true);
+			wait1Msec(10000);
+		 	while((getMotorTargetCompleted(rightMotor) == false || getMotorTargetCompleted(leftMotor) == false)){}
+		}
+
+		//right turn
+		else if(direction == right)
+		{
+			moveMotorTarget(rightMotor,0,0,true);
+			moveMotorTarget(leftMotor,(int)result,power,true); //moveMotorTarget takes int for distance therefore the (int) casting
+			wait1Msec(10000);
+			while((getMotorTargetCompleted(rightMotor) == false || getMotorTargetCompleted(leftMotor) == false)){}
+		}
+	}
+
 task main() {
 
-	startTask(UARTX_recv);
+	//startTask(sendData);
+	char tmp;
+	int check = 1;
 
+	while(check == 1)
+	{
+		tmp = UARTX_recv();
+
+		if(tmp == 'a')
+		{
+			wait1Msec(2000);
+			forwardMovement(500, 40);
+			//openClaw();
+			//followLineUntilObject(15, 8);
+			//wait1Msec(4000);
+			//closeClaw();
+			//raiseArm(30);
+			check = 0;
+		}else if(tmp == 'b')
+		{
+			lowerArm(30);
+		}
+	}
 }
